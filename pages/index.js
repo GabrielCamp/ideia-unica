b7e02983a50c1bd002887620e7baea09df90511a
Habimael <gabriel.camposdasilva@hotmail.com>
2021-01-14 18:48:48 -0300

    home nova 2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
     return (
     <div>
-        <h1>Eu Te Amo Stéfany Gomes Sena Campos</h1>
+        <h1>Eu Te Amo</h1>
+        <h3>Stéfany Gomes Sena Campos</h3>
+        <h6>De: Gabriel Campos da Silva.</h6>
         <small>copyright 2020, Todos os direitos reservados, Jaguaré ES.</small>
     </div>
     ) 
