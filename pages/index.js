181e65b16d751bd304819fc51af26970dbd44c63
Habimael <gabriel.camposdasilva@hotmail.com>
2021-01-15 13:13:26 -0300

    ideia-unica

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
+<script>
+var time = new Date();
+console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+</script>
+
 function Home() {
     return (
     <div>
-        <h1>Eu Te Amo</h1>
-        <h3>Stéfany Gomes Sena Campos</h3>
-        <h6>De: Gabriel Campos da Silva.</h6>
-        <small>copyright 2020, Todos os direitos reservados, Jaguaré ES.</small>
+        <h1>Meu Irmãozinho mais querido!</h1>   
+        <p>DAVID CAMPOS DA SILVA</p>
+        <img src="david.JPG">David Campos da Silva</img>
+    <small>copyright: Gabriel Campos da Silva</small>
     </div>
-    ) 
+    )        
 }
+
+<script>
+let d = new Date();
+document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
+"</h1>"
+</script>
 
 export default Home
