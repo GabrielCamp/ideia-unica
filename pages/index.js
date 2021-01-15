94e1ed28172c1030233fb98df96bca29e4455de6
Habimael <gabriel.camposdasilva@hotmail.com>
2021-01-15 13:17:34 -0300

    ideiaBoa

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,3 @@
-<script>
-var time = new Date();
-console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
-</script>
-
 function Home() {
     return (
     <div>
@@ -13,11 +8,5 @@ function Home() {
     </div>
     )        
 }
-
-<script>
-let d = new Date();
-document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
-"</h1>"
-</script>
 
 export default Home
