5d0a2609439dbe8447a7cac40884cd0592e60ae2
Habimael <gabriel.camposdasilva@hotmail.com>
2021-01-15 13:20:17 -0300

    unicaIdeia

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ function Home() {
         <h1>Meu Irmãozinho mais querido!</h1>   
         <p>DAVID CAMPOS DA SILVA</p>
         <img src="david.JPG">David Campos da Silva</img>
-    <small>copyright: Gabriel Campos da Silva</small>
     </div>
     )        
 }
